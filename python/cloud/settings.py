bb9fb6505056b73fda8fdce3ac6079d1c72225e3
yiakwy <yiak.wy@gmail.com>
2019-06-28 16:15:31 +0800

    modify ROOT path

--- python/cloud/settings.py
+++ python/cloud/settings.py
@@ -4,10 +4,10 @@ import sys
 ROOT = os.path.dirname(os.path.dirname(__file__))
 
 # dataset
-DATA_DIR = os.path.abspath(os.path.join(ROOT, "../../", "data"))
+DATA_DIR = os.path.abspath(os.path.join(ROOT, "../", "data"))
 
 # output
-OUTPUT_DIR = os.path.abspath(os.path.join(ROOT, "../../", "output"))
+OUTPUT_DIR = os.path.abspath(os.path.join(ROOT, "../", "output"))
 
 # pretrained model
 CAT_DOG_PRETRAINED_MODEL = os.path.join(DATA_DIR, "pretrained", "dog_cat_dataset/weights.best.checkpoint.hdf5")
